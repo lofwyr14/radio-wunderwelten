760ced3d76ae7a5d720d52461d328872793858cc
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2020-04-11 13:22:51 +0200

    Ton Art Playlisten Mehr Attribute für Songs Fehlertoleranz

--- src/main/www/js/index.js
+++ src/main/www/js/index.js
@@ -3,6 +3,7 @@
   factory();
 }((function () { 'use strict';
 
+  // import {html, render, TemplateResult} from "lit-html";
   class Broadcast {
       constructor(broadcast) {
           this.id = broadcast.id;
@@ -17,78 +18,149 @@
       constructor(episode) {
           this.id = episode.id;
           this.title = episode.title;
-          this.date = new Date(Date.parse(episode.date));
-          this.songs = [];
-          episode.songs.forEach(entry => {
-              entry.forEach(song => this.songs.push(new Song(song)));
-          });
+          this.date = episode.date ? new Date(Date.parse(episode.date)) : null;
+          this.songs = new SongList(episode.songs);
       }
       get dateFormat() {
-          const month = this.date.getUTCMonth() + 1;
-          return this.date.getUTCDate() + "." + (month > 9 ? "" : "0") + month + "." + this.date.getUTCFullYear();
+          if (this.date) {
+              const month = this.date.getUTCMonth() + 1;
+              return this.date.getUTCDate() + "." + (month > 9 ? "" : "0") + month + "." + this.date.getUTCFullYear();
+          }
+          else {
+              return "unbekannt";
+          }
       }
   }
   class Song {
       constructor(song) {
           this.title = song.title;
           this.performer = song.performer;
+          this.composer = song.composer;
+          this.number = song.number;
+          this.album = song.album;
+          this.genre = song.genre;
+          this.year = song.year;
+      }
+  }
+  class SongList {
+      constructor(object) {
+          this.list = [];
+          this.hasTitle = false;
+          this.hasPerformer = false;
+          this.hasComposer = false;
+          this.hasNumber = false;
+          this.hasAlbum = false;
+          this.hasGenre = false;
+          this.hasYear = false;
+          object === null || object === void 0 ? void 0 : object.forEach(entry => {
+              entry === null || entry === void 0 ? void 0 : entry.forEach(object => {
+                  const song = new Song(object);
+                  this.list.push(song);
+                  if (song.title) {
+                      this.hasTitle = true;
+                  }
+                  if (song.performer) {
+                      this.hasPerformer = true;
+                  }
+                  if (song.composer) {
+                      this.hasComposer = true;
+                  }
+                  if (song.number) {
+                      this.hasNumber = true;
+                  }
+                  if (song.album) {
+                      this.hasAlbum = true;
+                  }
+                  if (song.genre) {
+                      this.hasGenre = true;
+                  }
+                  if (song.year) {
+                      this.hasYear = true;
+                  }
+              });
+          });
       }
   }
   class Radio extends HTMLElement {
       constructor() {
           super();
-          console.log("Song");
+          console.info("Radio constructor!");
       }
       connectedCallback() {
-          // this.renderSonglist();
           this.renderBroadcast();
+          // const template = this.test("Yeah!");
+          // render(template, document.body);
       }
-      renderSonglist() {
-          this.insertAdjacentHTML("afterbegin", `<table class="table"><tbody></tbody></table>`);
-          this.getSongs().then(songs => {
-              const tbody = this.querySelector("table>tbody");
-              songs.forEach(measuring => {
-                  tbody.insertAdjacentHTML("beforeend", `<tr><td>${measuring.title}</td><td>${measuring.performer}</td></tr>`);
-              });
-          });
+      get space() {
+          return this.getAttribute("space");
       }
+      set space(space) {
+          this.setAttribute("space", space);
+      }
+      // private test(name: string): TemplateResult {
+      //   return html`<h1>Wochenende: ${name}</h1>`
+      // }
       renderBroadcast() {
           this.insertAdjacentHTML("afterbegin", `<div class="form-group">
 <label>Songliste vom ...
-<select class="form-control"></select></label></div>
-<table class="table"><thead>
-  <tr>
-    <th>Song</th>
-    <th>Interpret</th>
-  </tr></thead><tbody></tbody></table>`);
-          this.getBroadcast("json/zwwdp.json").then(broadcast => {
+<select class="form-control"></select></label></div>`);
+          // const location = "json/ta.json";
+          const location = `json/${this.space}.json`;
+          this.getBroadcast(location).then(broadcast => {
               const select = this.querySelector("select");
               select.addEventListener("change", (event) => {
                   const optionalParams = event.currentTarget;
                   console.log("test %o", optionalParams);
                   console.log("test %o", optionalParams.value);
-                  const table = this.querySelector("tbody");
-                  table.innerHTML = "";
                   const id = optionalParams.value;
+                  const oldTable = this.querySelector("table");
+                  if (oldTable) {
+                      this.removeChild(oldTable); // Alte Tabelle weg, todo: besser mit LIT HTML
+                  }
                   if (id) {
                       const episode = broadcast.episodes.get(id);
-                      episode.songs.forEach(song => {
-                          table.insertAdjacentHTML("beforeend", `<tr><td>${song.title}</td><td>${song.performer}</td></tr>`);
+                      this.insertAdjacentHTML("beforeend", `<table class="table table-striped">
+<colgroup>
+<col/>
+<col/>
+<col/>
+<col/>
+<col/>
+<col/>
+<col/>
+</colgroup>
+<thead class="thead-dark">
+  <tr>
+    <th class="${episode.songs.hasTitle ? '' : 'd-none'}">Titel</th>
+    <th class="${episode.songs.hasPerformer ? '' : 'd-none'}">Interpret</th>
+    <th class="${episode.songs.hasComposer ? '' : 'd-none'}">Komponist</th>
+    <th class="${episode.songs.hasNumber ? '' : 'd-none'}">Nummer</th>
+    <th class="${episode.songs.hasAlbum ? '' : 'd-none'}">Album</th>
+    <th class="${episode.songs.hasGenre ? '' : 'd-none'}">Genre</th>
+    <th class="${episode.songs.hasYear ? '' : 'd-none'}">Jahr</th>
+  </tr></thead><tbody></tbody></table>`);
+                      const table = this.querySelector("tbody");
+                      table.innerHTML = "";
+                      episode.songs.list.forEach(song => {
+                          table.insertAdjacentHTML("beforeend", `<tr>
+<td class="${episode.songs.hasTitle ? '' : 'd-none'}">${song.title ? song.title : ""}</td>
+<td class="${episode.songs.hasPerformer ? '' : 'd-none'}">${song.performer ? song.performer : ""}</td>
+<td class="${episode.songs.hasComposer ? '' : 'd-none'}">${song.composer ? song.composer : ""}</td>
+<td class="${episode.songs.hasNumber ? '' : 'd-none'}">${song.number ? song.number : ""}</td>
+<td class="${episode.songs.hasAlbum ? '' : 'd-none'}">${song.album ? song.album : ""}</td>
+<td class="${episode.songs.hasGenre ? '' : 'd-none'}">${song.genre ? song.genre : ""}</td>
+<td class="${episode.songs.hasYear ? '' : 'd-none'}">${song.year ? song.year : ""}</td>
+</tr>`);
                       });
                   }
               });
               broadcast.episodes.forEach(episode => {
-                  select.insertAdjacentHTML("afterbegin", `<option value="${episode.id}">${episode.dateFormat} (${episode.songs.length} Titel)</td></option>`);
+                  select.insertAdjacentHTML("afterbegin", `<option value="${episode.id}">${episode.dateFormat} (${episode.songs.list.length} Titel)</td></option>`);
               });
               // Auswählen der Song-Tabelle für die 1. Episode
               select.selectedIndex = 0;
               select.dispatchEvent(new Event("change"));
           });
-      }
-      getSongs() {
-          return window.fetch("json/songs.json")
-              .then(response => response.json())
-              .then((json) => json.map((song) => new Song(song)));
       }
       getBroadcast(location) {
           return window.fetch(location)
--- src/main/www/ts/radio.ts
+++ src/main/www/ts/radio.ts
@@ -1,3 +1,5 @@
+// import {html, render, TemplateResult} from "lit-html";
+
 class Broadcast {
 
   id: string;
@@ -18,21 +20,22 @@ class Episode {
   id: string;
   title: string;
   date: Date;
-  songs: Song[];
+  songs: SongList;
 
   constructor(episode: any) {
     this.id = episode.id;
     this.title = episode.title;
-    this.date = new Date(Date.parse(episode.date));
-    this.songs = [];
-    episode.songs.forEach(entry => {
-      entry.forEach(song => this.songs.push(new Song(song)));
-    })
+    this.date = episode.date ? new Date(Date.parse(episode.date)) : null;
+    this.songs = new SongList(episode.songs);
   }
 
   get dateFormat(): string {
-    const month = this.date.getUTCMonth() + 1;
-    return this.date.getUTCDate() + "." + (month > 9 ? "" : "0") + month + "." + this.date.getUTCFullYear();
+    if (this.date) {
+      const month = this.date.getUTCMonth() + 1;
+      return this.date.getUTCDate() + "." + (month > 9 ? "" : "0") + month + "." + this.date.getUTCFullYear();
+    } else {
+      return "unbekannt";
+    }
   }
 }
 
@@ -40,10 +43,62 @@ class Song {
 
   title: string;
   performer: string;
+  composer: string;
+  number: string;
+  album: string;
+  genre: string;
+  year: string;
 
   constructor(song: any) {
     this.title = song.title;
     this.performer = song.performer;
+    this.composer = song.composer;
+    this.number = song.number;
+    this.album = song.album;
+    this.genre = song.genre;
+    this.year = song.year;
+  }
+}
+
+class SongList {
+
+  list: Song[] = [];
+  hasTitle: boolean = false;
+  hasPerformer: boolean = false;
+  hasComposer: boolean = false;
+  hasNumber: boolean = false;
+  hasAlbum: boolean = false;
+  hasGenre: boolean = false;
+  hasYear: boolean = false;
+
+  constructor(object) {
+    object?.forEach(entry => {
+      entry?.forEach(object => {
+        const song = new Song(object);
+        this.list.push(song);
+        if (song.title) {
+          this.hasTitle = true;
+        }
+        if (song.performer) {
+          this.hasPerformer = true;
+        }
+        if (song.composer) {
+          this.hasComposer = true;
+        }
+        if (song.number) {
+          this.hasNumber = true;
+        }
+        if (song.album) {
+          this.hasAlbum = true;
+        }
+        if (song.genre) {
+          this.hasGenre = true;
+        }
+        if (song.year) {
+          this.hasYear = true;
+        }
+      });
+    })
   }
 }
 
@@ -51,42 +106,37 @@ class Radio extends HTMLElement {
 
   constructor() {
     super();
-
-    console.log("Song");
-
+    console.info("Radio constructor!");
   }
 
   connectedCallback() {
-    // this.renderSonglist();
     this.renderBroadcast();
+
+    // const template = this.test("Yeah!");
+    // render(template, document.body);
   }
 
-  private renderSonglist() {
-    this.insertAdjacentHTML("afterbegin", `<table class="table"><tbody></tbody></table>`);
-
-    this.getSongs().then(
-        songs => {
-          const tbody = this.querySelector("table>tbody");
-          songs.forEach(measuring => {
-            tbody.insertAdjacentHTML("beforeend",
-                `<tr><td>${measuring.title}</td><td>${measuring.performer}</td></tr>`);
-          });
-        }
-    );
+  get space(): string {
+    return this.getAttribute("space");
   }
+
+  set space(space: string) {
+    this.setAttribute("space", space);
+  }
+
+  // private test(name: string): TemplateResult {
+  //   return html`<h1>Wochenende: ${name}</h1>`
+  // }
 
   private renderBroadcast() {
     this.insertAdjacentHTML("afterbegin",
         `<div class="form-group">
 <label>Songliste vom ...
-<select class="form-control"></select></label></div>
-<table class="table"><thead>
-  <tr>
-    <th>Song</th>
-    <th>Interpret</th>
-  </tr></thead><tbody></tbody></table>`);
+<select class="form-control"></select></label></div>`);
 
-    this.getBroadcast("json/zwwdp.json").then(
+    // const location = "json/ta.json";
+    const location = `json/${this.space}.json`;
+    this.getBroadcast(location).then(
         broadcast => {
           const select = this.querySelector("select");
           select.addEventListener("change",
@@ -95,17 +145,49 @@ class Radio extends HTMLElement {
                 console.log("test %o", optionalParams);
                 console.log("test %o", optionalParams.value);
 
-                const table = this.querySelector("tbody");
-                table.innerHTML = "";
 
                 const id = optionalParams.value;
 
+                const oldTable = this.querySelector("table");
+                if (oldTable) {
+                  this.removeChild(oldTable); // Alte Tabelle weg, todo: besser mit LIT HTML
+                }
                 if (id) {
                   const episode = broadcast.episodes.get(id);
-
-                  episode.songs.forEach(song => {
+                  this.insertAdjacentHTML("beforeend",
+                      `<table class="table table-striped">
+<colgroup>
+<col/>
+<col/>
+<col/>
+<col/>
+<col/>
+<col/>
+<col/>
+</colgroup>
+<thead class="thead-dark">
+  <tr>
+    <th class="${episode.songs.hasTitle ? '' : 'd-none'}">Titel</th>
+    <th class="${episode.songs.hasPerformer ? '' : 'd-none'}">Interpret</th>
+    <th class="${episode.songs.hasComposer ? '' : 'd-none'}">Komponist</th>
+    <th class="${episode.songs.hasNumber ? '' : 'd-none'}">Nummer</th>
+    <th class="${episode.songs.hasAlbum ? '' : 'd-none'}">Album</th>
+    <th class="${episode.songs.hasGenre ? '' : 'd-none'}">Genre</th>
+    <th class="${episode.songs.hasYear ? '' : 'd-none'}">Jahr</th>
+  </tr></thead><tbody></tbody></table>`);
+                  const table = this.querySelector("tbody");
+                  table.innerHTML = "";
+                  episode.songs.list.forEach(song => {
                     table.insertAdjacentHTML("beforeend",
-                        `<tr><td>${song.title}</td><td>${song.performer}</td></tr>`);
+                        `<tr>
+<td class="${episode.songs.hasTitle ? '' : 'd-none'}">${song.title ? song.title : ""}</td>
+<td class="${episode.songs.hasPerformer ? '' : 'd-none'}">${song.performer ? song.performer : ""}</td>
+<td class="${episode.songs.hasComposer ? '' : 'd-none'}">${song.composer ? song.composer : ""}</td>
+<td class="${episode.songs.hasNumber ? '' : 'd-none'}">${song.number ? song.number : ""}</td>
+<td class="${episode.songs.hasAlbum ? '' : 'd-none'}">${song.album ? song.album : ""}</td>
+<td class="${episode.songs.hasGenre ? '' : 'd-none'}">${song.genre ? song.genre : ""}</td>
+<td class="${episode.songs.hasYear ? '' : 'd-none'}">${song.year ? song.year : ""}</td>
+</tr>`);
                   });
                 }
 
@@ -113,7 +195,7 @@ class Radio extends HTMLElement {
               });
           broadcast.episodes.forEach(episode => {
             select.insertAdjacentHTML("afterbegin",
-                `<option value="${episode.id}">${episode.dateFormat} (${episode.songs.length} Titel)</td></option>`);
+                `<option value="${episode.id}">${episode.dateFormat} (${episode.songs.list.length} Titel)</td></option>`);
           });
 
           // Auswählen der Song-Tabelle für die 1. Episode
@@ -121,12 +203,6 @@ class Radio extends HTMLElement {
           select.dispatchEvent(new Event("change"));
         }
     );
-  }
-
-  getSongs(): Promise<Song[]> {
-    return window.fetch("json/songs.json")
-        .then(response => response.json())
-        .then((json: any[]) => json.map((song: any) => new Song(song)))
   }
 
   getBroadcast(location: string): Promise<Broadcast> {
