ae373b2a13bb397001d98ed80c3734f5d280dd38
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2020-04-16 08:33:10 +0200

    Vereinfachte Playlists Extraktion

--- src/main/resources/playlist/curl-rb2/zwwdp.sh
+++ src/main/resources/playlist/curl-rb2/zwwdp.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-date=2020-04-16
+date=2020-04-09
 
 curl -o zwwdp-${date}-1.html https://www.radiobremen.de/bremenzwei/musik/titelsuche/?wrapurl=%2Fbremenzwei%2Fmusik%2Ftitelsuche%2F\&selectdate=${date}\&stunde=22\&minute=20
 curl -o zwwdp-${date}-2.html https://www.radiobremen.de/bremenzwei/musik/titelsuche/?wrapurl=%2Fbremenzwei%2Fmusik%2Ftitelsuche%2F\&selectdate=${date}\&stunde=22\&minute=55
 curl -o zwwdp-${date}-3.html https://www.radiobremen.de/bremenzwei/musik/titelsuche/?wrapurl=%2Fbremenzwei%2Fmusik%2Ftitelsuche%2F\&selectdate=${date}\&stunde=23\&minute=30
 
-cat zwwdp-${date}-1.html zwwdp-${date}-2.html zwwdp-${date}-3.html > zwwdp-${date}.html
+cat zwwdp-${date}-1.html zwwdp-${date}-2.html zwwdp-${date}-3.html | sed s/style\=\".*\"// > zwwdp-${date}.html
+echo \<script\> >> zwwdp-${date}.html
+cat zwwdp.js >> zwwdp-${date}.html
+echo \</script\> >> zwwdp-${date}.html
