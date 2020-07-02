fad2b4a3b5e28e632328abd68a2f9936f037e23d
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2020-07-02 06:24:48 +0200

    Fiehe Juni

--- src/main/resources/playlist/fiehe/fiehe.sh
+++ src/main/resources/playlist/fiehe/fiehe.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for i in `seq 1 613`;
+for i in `seq 635 650`;
 do
   echo ${i}
   curl -v -o fiehe-${i}.html --fail https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/fiehe-${i}.html
