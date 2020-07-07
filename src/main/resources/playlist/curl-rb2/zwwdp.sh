b762f8ee91da8e779affc4bfda88a81f003b34a2
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2020-07-07 06:32:28 +0200

    Zeigler...

--- src/main/resources/playlist/curl-rb2/zwwdp.sh
+++ src/main/resources/playlist/curl-rb2/zwwdp.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-date=2020-06-25
+date=2020-07-02
+date=2020-07-09
 
 curl -o zwwdp-${date}-1.html https://www.radiobremen.de/bremenzwei/musik/titelsuche/?wrapurl=%2Fbremenzwei%2Fmusik%2Ftitelsuche%2F\&selectdate=${date}\&stunde=22\&minute=22
 curl -o zwwdp-${date}-2.html https://www.radiobremen.de/bremenzwei/musik/titelsuche/?wrapurl=%2Fbremenzwei%2Fmusik%2Ftitelsuche%2F\&selectdate=${date}\&stunde=23\&minute=00
