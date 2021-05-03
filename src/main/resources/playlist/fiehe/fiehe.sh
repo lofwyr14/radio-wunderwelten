a01348ea171ad9505ebf3e41e0b160591b0a3509
jogerjet <joerg.gerjets@gmx.de>
2021-05-03 20:31:16 +0200

    upload fiehe playlist 2021-04-25 checked fiehe 2021-04-18

--- src/main/resources/playlist/fiehe/fiehe.sh
+++ src/main/resources/playlist/fiehe/fiehe.sh
@@ -3,7 +3,7 @@
 # "Archive"
 # https://web.archive.org/web/20191105232532/https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/index.html
 
-for id in `seq 736 736`;
+for id in `seq 738 738`;
 do
   echo ${id}
   curl -v -o fiehe-${id}.html --fail https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/fiehe-${id}.html
