c7c6e3a047d35954d180338fa4a48bac4f1dc90b
jogerjet <joerg.gerjets@gmx.de>
2021-08-13 13:29:34 +0200

    upload fiehe 2021-08-01

--- src/main/resources/playlist/fiehe/fiehe.sh
+++ src/main/resources/playlist/fiehe/fiehe.sh
@@ -3,10 +3,12 @@
 # "Archive"
 # https://web.archive.org/web/20191105232532/https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/index.html
 
-for id in `seq 768 768`;
+for id in `seq 770 770`;
 do
   echo ${id}
   curl -v -o fiehe-${id}.html --fail https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/fiehe-${id}.html
+  # alt, funktioniert aber noch: https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/fiehe-${id}.html
+  # neu auf geänderter 1live-Webseite: https://www1.wdr.de/radio/1live/musik/1live-fiehe/fiehe-${id}.html
   if [[ -e fiehe-${id}.html ]]; then
     echo \<script\> >> fiehe-${id}.html
     cat fiehe.js >> fiehe-${id}.html
