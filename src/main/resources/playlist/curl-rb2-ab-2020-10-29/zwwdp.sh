e9ef4869bfc34ecd0a11c7be3d9f20b6175794c4
jogerjet <joerg.gerjets@gmx.de>
2022-03-13 11:43:04 +0100

    upload zeigler 2022-03-10

--- src/main/resources/playlist/curl-rb2-ab-2020-10-29/zwwdp.sh
+++ src/main/resources/playlist/curl-rb2-ab-2020-10-29/zwwdp.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 # vermutlich immer um 2 erhöhen
-ID=252
+ID=256
 
 # aktuelles Jahr
 YEAR=2022
 # muss jedes Jahr und beim Ausfall einer Sendung angepasst werden
-IDWEEKOFFSET=117
+IDWEEKOFFSET=118
 # 4 = Donnerstags
 DAYOFWEEK=4
 
