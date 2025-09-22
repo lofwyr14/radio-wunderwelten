b20f6792d84df591abaaeff3c9ae39dfbbe5e3e4
jogerjet <joerg.gerjets@gmx.de>
2025-09-22 21:28:13 +0200

    upload fiehe 2025-09-14

--- src/main/resources/playlist/fiehe/fiehe.sh
+++ src/main/resources/playlist/fiehe/fiehe.sh
@@ -4,7 +4,7 @@
 # https://web.archive.org/web/20191105232532/https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/index.html
 
 # for id in `seq 798 798`;
-for id in `seq 1212 1212`;
+for id in `seq 1214 1214`;
 do
   echo ${id}
   curl -v -o fiehe-${id}.html --fail https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/fiehe-${id}.html
